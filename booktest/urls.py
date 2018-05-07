98488f2f59f597d3ff608a3eb190a2db3a5e9ef1
bwylove <bwylove@126.com>
2018-05-07 11:22:00 +0800

    视图

---
+++ booktest/urls.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+from django.conf.urls import url
+import views
+urlpatterns = [
+    url(r'^$',views.index,name='index'),
+    url(r'^(\d+)$',views.detail,name='detail'),
+# 正则表达式非命名组，通过位置参数传递给视图
+    url(r'^(\d+)/(\d+)/(\d+)/$',views.detail2,name='detail2'),
+# 正则表达式命名组，通过关键字参数传递给视图
+    url(r'^(?P<p2>\d+)-(?P<p3>\d+)-(?P<p1>\d+)/$',views.detail3,name='detail3'),
+    url(r'^getTest1/$',views.getTest1),
+    url(r'^getTest2/$',views.getTest2),
+    url(r'^getTest3/$',views.getTest3),
+]
--- booktest/views.py
+++ booktest/views.py
@@ -2,5 +2,41 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    return HttpResponse('hello world')
+
+
+def detail(request,p1):
+    return HttpResponse(p1)
+
+# def show(*args,**kwargs):
+# #     # args 以位置对应的  元组
+# #     # kwargs 以关键字对应的  字典
+def detail2(request,p1,p2,p3):
+    return HttpResponse('year:%s,month:%s,day:%s'%(p1,p2,p3))
+
+
+def detail3(request,p1,p2,p3):
+    return HttpResponse('year:%s,month:%s,day:%s'%(p1,p2,p3))
+#展示链接的页面
+def getTest1(request):
+    return render(request,'booktest/getTest1.html')
+
+#展示一键一值的情况
+def getTest2(request):
+    #根据键接受值
+    a1=request.GET['a']
+    b1=request.GET['b']
+    c1=request.GET['c']
+    # 构造上下文
+    context={'a':a1,'b':b1,'c':c1}
+    # 向模板传递上下文，并进行渲染
+    return render(request,'booktest/getTest2.html',context)
+
+#展示一键多值的情况
+def getTest3(request):
+    a1=request.GET.getlist('a')
+    context={'a':a1}
+    return render(request,'booktest/getTest3.html',context)
